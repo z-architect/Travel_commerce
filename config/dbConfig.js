ce7b3bcec999e5ec04f2a1cb2beaeae47a748efb
Z-architect <davidog242@gmail.com>
2020-09-28 14:45:54 +0300

    researched cookie parsing and post man endpoint test#1

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,3 +1,5 @@
 module.exports = {
-    url : "mongodb://127.0.0.1:27017/<dbname>"
+    url : "mongodb://127.0.0.1:27017/nutella"
 }
+
+//<last part is db name>
--- server.js
+++ server.js
@@ -1,9 +1,38 @@
 const express = require("express")
-app = express();
+const dbConfig = require("./config/dbConfig")
+const mongoose = require("mongoose")
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+const {User} = require('./config/dbModel');
 
+app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+mongoose.connect(dbConfig.url,{useNewUrlParser:true})
+		.then(()=>{console.log("succesful DB connection")})
+		.catch(err=>{
+			console.log("Yhea something went wrong with database")
+		})
 
 app.get('/',(req,res)=>{
+	res.status(200);
+	res.send({name:"itsa working",
+			  age:"not sur if thsi gon work"})
     res.send("hello world");
+})
+app.post('/',(req,res)=>{
+	console.log(req.body);
+	res.send("got it");
+})
+app.post('/api/users/register',(req,res)=>{
+	const user = new User(req.body);
+
+	user.save((err,userData)=>{
+		if(err) return res.json({success: false,err})
+	});
+	return res.status(200).json({sucess:true});
 })
 
 app.listen(5000,()=>{
